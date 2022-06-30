41610e270e96cf420ca5aa6474f6c25538e3e53f
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-06-30 18:07:08 -0300

    layout fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import RocketseatLogo from "./RocketseatLogo";
 function Footer() {
   return (
-    <footer className="bg-gray-900 flex justify-center">
+    <footer className="bg-gray-900 w-full flex justify-center">
       <div className="max-w-[1100px] w-full">
         <div className=" flex flex-col items-center gap-6 py-4 border-t border-gray-500 text-gray-300 text-sm mx-4 tablet:mx-8 large-tablet:flex-row large-tablet:justify-between">
           <div className="flex flex-col items-center gap-6 large-tablet:flex-row">
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -26,62 +26,66 @@ function Subscribe() {
   };
 
   return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex mobile:flex-col px-6 items-center">
-      <div className="max-w-[1100px] w-full mobile:flex mobile:flex-col laptop:flex-row tablet:mt-20 justify-between items-center">
-        <div className="max-w-[635px] flex flex-col mobile:items-center laptop:items-start gap-8 mobile:my-12">
-          <Logo width={237} />
-          <h1 className="block mobile:text-3xl tablet:text-5xl mobile:text-center laptop:text-left text-[2.5rem] leading-tight">
-            Construa uma{" "}
-            <strong className="text-blue-500">aplicação completa</strong>, do
-            zero, com <strong className="text-blue-500">ReactJs</strong>
-          </h1>
-          <p className="text-gray-200  leading-relaxed mobile:text-center laptop:text-left">
-            Em apenas uma semana você vai dominar na prática uma das tecnologias
-            mais utilizadas e com alta demanda para acessar as melhores
-            oportunidades do mercado.
-          </p>
-        </div>
-        <div className="mobile:w-screen tablet:w-fit p-8 bg-gray-700 border border-gray-500 rounded-lg">
-          <strong className="text-2xl text-gray-100 mb-6 block">
-            Inscreva-se gratuitamente
-          </strong>
-          <form
-            onSubmit={handleSubscribe}
-            className="flex flex-col items-center gap-2 w-full"
-          >
-            <input
-              type="text"
-              placeholder="Seu nome completo"
-              className="text-sm text-gray-300 bg-gray-900 py-4 px-5 w-full rounded border border-gray-500"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-            <input
-              type="email"
-              placeholder="Digite seu email"
-              className="text-sm text-gray-300 bg-gray-900 py-4 px-5 w-full rounded border border-gray-500"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <button
-              type="submit"
-              disabled={loading}
-              className="mt-4 py-4 font-bold bg-green-500 text-sm text-gray-100 uppercase w-full rounded hover:bg-green-700 transition-colors cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+    <div>
+      <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex mobile:flex-col px-6 items-center">
+        <div className="max-w-[1100px] w-full mobile:flex mobile:flex-col laptop:flex-row tablet:mt-20 justify-between items-center">
+          <div className="max-w-[635px] flex flex-col mobile:items-center laptop:items-start gap-8 mobile:my-12">
+            <Logo width={237} />
+            <h1 className="block mobile:text-3xl tablet:text-5xl mobile:text-center laptop:text-left text-[2.5rem] leading-tight">
+              Construa uma{" "}
+              <strong className="text-blue-500">aplicação completa</strong>, do
+              zero, com <strong className="text-blue-500">ReactJs</strong>
+            </h1>
+            <p className="text-gray-200  leading-relaxed mobile:text-center laptop:text-left">
+              Em apenas uma semana você vai dominar na prática uma das
+              tecnologias mais utilizadas e com alta demanda para acessar as
+              melhores oportunidades do mercado.
+            </p>
+          </div>
+          <div className="mobile:w-screen tablet:w-fit p-8 bg-gray-700 border border-gray-500 rounded-lg">
+            <strong className="text-2xl text-gray-100 mb-6 block">
+              Inscreva-se gratuitamente
+            </strong>
+            <form
+              onSubmit={handleSubscribe}
+              className="flex flex-col items-center gap-2 w-full"
             >
-              Garantir minha vaga
-            </button>
-          </form>
+              <input
+                type="text"
+                placeholder="Seu nome completo"
+                className="text-sm text-gray-300 bg-gray-900 py-4 px-5 w-full rounded border border-gray-500"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <input
+                type="email"
+                placeholder="Digite seu email"
+                className="text-sm text-gray-300 bg-gray-900 py-4 px-5 w-full rounded border border-gray-500"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className="mt-4 py-4 font-bold bg-green-500 text-sm text-gray-100 uppercase w-full rounded hover:bg-green-700 transition-colors cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Garantir minha vaga
+              </button>
+            </form>
+          </div>
         </div>
+        <img
+          src="/src/assets/images/cold-mockup.png"
+          className="mt-10 max-w-[1100px] w-full"
+          alt=""
+          onContextMenu={(e) => e.preventDefault()}
+          onDragStart={(e) => e.preventDefault()}
+        />
       </div>
-      <img
-        src="/src/assets/images/cold-mockup.png"
-        className="mt-10 max-w-[1100px] w-full"
-        alt=""
-      />
       <Footer />
     </div>
   );
